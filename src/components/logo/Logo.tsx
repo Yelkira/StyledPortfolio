fbe90b495261150914800766e95b233e599a542b
Yelkira <penda.fu@gmail.com>
2023-07-10 15:25:45 +0300

    Add logo component

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {HtmlIcon} from "../../assets/icons/IconHtml";
+
+export const Logo = () => {
+    return (
+        <a href="/">
+            <HtmlIcon/>
+        </a>
+    );
+};
+
