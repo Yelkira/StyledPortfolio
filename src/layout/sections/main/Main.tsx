2b009941c6576ba36996cbf893e4ea96ee93222e
Yelkira <penda.fu@gmail.com>
2023-07-10 16:29:07 +0300

    create Main component

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/portrait 1.png'
+import {FlexContainer} from "../../../components/FlexContainer";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexContainer align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi There</span>
+                    <Name>I am Super Toxic</Name>
+                    <MainTitle>A Web Developer.</MainTitle>
+                </div>
+                <Photo src={photo} alt="My photo"/>
+            </FlexContainer>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #99ec3f;
+`
+
+const Photo = styled.img`
+ width: 350px;
+  height: 430px;
+`
+const MainTitle = styled.h1`
+  color: green;
+`
+const Name = styled.h2`
+  color: #040080;
+`
