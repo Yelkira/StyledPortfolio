baa0c8c31da2a1cb9f3d4bb6108c98f08c0c1051
Yelkira <penda.fu@gmail.com>
2023-07-19 12:49:31 +0300

    refactor header component

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import styled from "styled-components";
+import {Container} from "../../components/Container";
+import {FlexContainer} from "../../components/FlexContainer";
 import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
 
-const items = ["Home", "Skills", "Works","Testimony", "Contact"]
+const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexContainer justify={'space-between'} align={'center'}>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                </FlexContainer>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
   background-color: #aff199;
-  display: flex;
-  justify-content: space-between;
 `
