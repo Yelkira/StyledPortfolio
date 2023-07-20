cf3b59a5f3234e9b7a25c055d0c19d80c296565f
Yelkira <penda.fu@gmail.com>
2023-07-20 15:25:12 +0300

    styling Skills and Skill

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {CssIcon} from "../../../assets/icons/IconCSS";
 import {HtmlIcon} from "../../../assets/icons/IconHtml";
+import {Container} from "../../../components/Container";
 import {FlexContainer} from "../../../components/FlexContainer";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Skill} from "./skill/Skill";
@@ -9,39 +10,39 @@ import {Skill} from "./skill/Skill";
 export const Skills = () => {
     return (
         <StyledSkills>
-            <SectionTitle>
-                My skills
-            </SectionTitle>
-            <FlexContainer wrap={'wrap'}>
-                <Skill Icon={<HtmlIcon/>}
-                       title={'HTML'}
-                       text={"Lorem ito at consequatur dicta dolores harum incidunt, itaque magni modi necessitatibus odio officiis perferendis quia quod repellat, similique voluptates."}/>
-                <Skill Icon={<CssIcon/>}
-                       title={'HTML'}
-                       text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam architecto ."}/>
-                <Skill Icon={<HtmlIcon/>}
-                       title={'HTML'}
-                       text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam arctibus odio officiis perferendis quia quod repellat, similique voluptates."}
-                />
-                <Skill Icon={<HtmlIcon/>}
-                       title={'HTML'}
-                       text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam arctibus odio officiis perferendis quia quod repellat, similique voluptates."}
-                />
-                <Skill Icon={<HtmlIcon/>}
-                       title={'HTML'}
-                       text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam arctibus odio officiis perferendis quia quod repellat, similique voluptates."}
-                />
-                <Skill Icon={<HtmlIcon/>}
-                       title={'HTML'}
-                       text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam arctibus odio officiis perferendis quia quod repellat, similique voluptates."}
-                />
-            </FlexContainer>
+            <Container>
+                <SectionTitle>
+                    My skills
+                </SectionTitle>
+                <FlexContainer wrap={'wrap'}>
+                    <Skill Icon={<HtmlIcon/>}
+                           title={'HTML'}
+                           text={"Lorem ito at consequatur dicta dolores harum incidunt, itaque magni modi necessitatibus odio officiis perferendis quia quod repellat, similique voluptates."}/>
+                    <Skill Icon={<CssIcon/>}
+                           title={'HTML'}
+                           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam architecto ."}/>
+                    <Skill Icon={<HtmlIcon/>}
+                           title={'HTML'}
+                           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam arctibus odio officiis perferendis quia quod repellat, similique voluptates."}
+                    />
+                    <Skill Icon={<HtmlIcon/>}
+                           title={'HTML'}
+                           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam arctibus odio officiis perferendis quia quod repellat, similique voluptates."}
+                    />
+                    <Skill Icon={<HtmlIcon/>}
+                           title={'HTML'}
+                           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam arctibus odio officiis perferendis quia quod repellat, similique voluptates."}
+                    />
+                    <Skill Icon={<HtmlIcon/>}
+                           title={'HTML'}
+                           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam arctibus odio officiis perferendis quia quod repellat, similique voluptates."}
+                    />
+                </FlexContainer>
+            </Container>
         </StyledSkills>
     );
 };
 
 const StyledSkills = styled.section`
   background-color: #ffe5e4;
-  min-height: 100vh;
-
 `
--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,26 +1,66 @@
 import React, {ReactNode} from 'react';
 import styled from "styled-components";
+import {FlexContainer} from "../../../../components/FlexContainer";
+import {theme} from "../../../../styles/Theme";
 
 type SkillPropsType = {
     Icon: ReactNode
-    title:string
-    text:string
+    title: string
+    text: string
 }
 
 
 export const Skill = ({Icon, title, text}: SkillPropsType) => {
     return (
         <StyledSkill>
-            {Icon}
-            <SkillTitle>{title}</SkillTitle>
-            <SkillText>{text}</SkillText>
+            <FlexContainer direction={'column'} align={'center'}>
+                <IconWrapper>
+                    {Icon}
+                </IconWrapper>
+                <SkillTitle>{title}</SkillTitle>
+                <SkillText>{text}</SkillText>
+            </FlexContainer>
         </StyledSkill>
     );
 };
 
 const StyledSkill = styled.div`
-  width: 33%;
-  background-color: #e88f8f;
+  width: 380px;
+  padding: 62px 20px 40px;
 `
-const SkillTitle = styled.h3``
-const SkillText = styled.p``
+const IconWrapper = styled.div`
+  position: relative;
+  z-index: 1;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    transform: rotate(45deg) translate(-50%, -50%);
+    background-color: rgba(255, 255, 255, 0.1);
+    left: 50%;
+    top: 50%;
+    transform-origin: top left;
+    
+
+    position: absolute;
+    z-index: -1;
+  }
+
+`
+
+const SkillTitle = styled.h3`
+  margin: 70px 0 15px;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 700;
+  font-size: 16px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+
+`
+const SkillText = styled.p`
+  text-align: center;
+  font-weight: 400;
+  font-size: 14px;
+`
