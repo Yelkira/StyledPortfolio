8ac0ed932dc48201539d57aa57b4e0cd4eaed539
Yelkira <penda.fu@gmail.com>
2023-07-20 15:24:24 +0300

    add padding to GlobalStyles.ts

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -32,6 +32,10 @@ export const GlobalStyles = createGlobalStyle`
     border:none;
   }
   
+  section{
+    padding: 100px 0;
+  }
+  
   section:nth-of-type(odd){
     background-color: ${theme.colors.primaryBg};
   }
