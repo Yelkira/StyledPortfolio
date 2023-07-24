30f6281b81b2e84788fa7fad9223126f9bc20c18
Yelkira <penda.fu@gmail.com>
2023-07-24 13:41:55 +0300

    styling slogan component

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import styled from "styled-components";
 import {Button} from "../../../components/button/Button";
+import {Container} from "../../../components/Container";
+import {FlexContainer} from "../../../components/FlexContainer";
 import {SectionTitle} from "../../../components/SectionTitle";
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-                <SectionTitle>I Am Available For Freelance</SectionTitle>
-                <Button>Hire me</Button>
+            <Container>
+                <FlexContainer direction={'column'} justify={'center'} align={'center'}>
+                    <SectionTitle>I Am Available For Freelance</SectionTitle>
+                    <Button>Hire me</Button>
+                </FlexContainer>
+            </Container>
         </StyledSlogan>
     );
 };
 
 const StyledSlogan = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 30vh;
-  background-color: darkred;
 `
